9425df0139a5441936b85f671096865ab5eb2e55
deadmeats <deadmeats@hotmail.com>
2018-08-18 20:26:59 -0300

    qXX X X
    feqwe!
     Please enter the commit message for your changes. Lines startin
     with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   jarmaker/.classpath
    	bnn file:   jarmaker/.project
    	new file:   jarmaker/.settings/org.eclipse.jdt.core.prefs
    	new file:   jarmaker/bin/run/AWTCounter.class
    	new file:   jarmaker/src/run/AWTCounter.java

---
+++ jarmaker/src/run/AWTCounter.java
@@ -0,0 +1,116 @@
+package run;
+
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+ 
+// An AWT program inherits from the top-level container java.awt.Frame
+public class AWTCounter extends Frame implements ActionListener, WindowListener {
+	private Label lblCount;    // Declare a Label component 
+	private TextField tfCount; // Declare a TextField component 
+	private Button btnCount;   // Declare a Button component
+	private int count = 0;     // Counter's value
+
+	// Constructor to setup GUI components and event handlers
+	public AWTCounter () {
+		setLayout(new FlowLayout());
+		// "super" Frame, which is a Container, sets its layout to FlowLayout to arrange
+		// the components from left-to-right, and flow to next row from top-to-bottom.
+
+		lblCount = new Label("Counter");  // construct the Label component
+		add(lblCount);                    // "super" Frame container adds Label component
+
+		tfCount = new TextField(count + "", 10); // construct the TextField component with initial text
+		tfCount.setEditable(false);       // set to read-only
+		add(tfCount);                     // "super" Frame container adds TextField component
+
+		btnCount = new Button("Count");   // construct the Button component
+		add(btnCount);                    // "super" Frame container adds Button component
+
+		btnCount.addActionListener(this);
+		// "btnCount" is the source object that fires an ActionEvent when clicked.
+		// The source add "this" instance as an ActionEvent listener, which provides
+		//   an ActionEvent handler called actionPerformed().
+		// Clicking "btnCount" invokes actionPerformed().
+		
+		addWindowListener(this);
+
+		setTitle("AWT Counter");  // "super" Frame sets its title
+		setSize(250, 100);        // "super" Frame sets its initial window size
+
+		// For inspecting the Container/Components objects
+		// System.out.println(this);
+		// System.out.println(lblCount);
+		// System.out.println(tfCount);
+		// System.out.println(btnCount);
+
+		setVisible(true);         // "super" Frame shows
+
+
+
+		// System.out.println(this);
+		// System.out.println(lblCount);
+		// System.out.println(tfCount);
+		// System.out.println(btnCount);
+	}
+
+	// The entry main() method
+	public static void main(String[] args) {
+		// Invoke the constructor to setup the GUI, by allocating an instance
+		AWTCounter app = new AWTCounter();
+		// or simply "new AWTCounter();" for an anonymous instance
+	}
+
+	// ActionEvent handler - Called back upon button-click.
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		++count; // Increase the counter value
+		// Display the counter value on the TextField tfCount
+		tfCount.setText(count + ""); // Convert int to String
+	}
+
+	// Window Listener interface
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	/* WindowEvent handlers */
+	// Called back upon clicking close-window button
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		System.exit(0);
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+}
